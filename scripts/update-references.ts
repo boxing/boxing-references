d609774b88ee252c06bb38a888dd6804d8955ce0
Mike DiDomizio <mikedidomizio@gmail.com>
2021-07-17 22:29:37 -0400

    Add badges to README

--- scripts/update-references.ts
+++ scripts/update-references.ts
@@ -1,13 +1,15 @@
-// const { exec } = require('child_process');
-import {exec} from 'child_process';
-// const {data} = require('../src/data');
+import {exec, execSync} from 'child_process';
 import {data} from '../src/data';
 
-console.log(data);
-
-exec("sed -i 's/Boxing%20References-#/Boxing%20References-5/g' README.md", (err, stdout, stderr) => {
-    if (err) {
-        console.log(err)
-        return;
+// we could just hardcode this value in, but Max Kellerman's rap career may not be over?!
+const numberOfMaxKellermanReferences = data.reduce((accumulator, currentValue) => {
+    if (currentValue.singer === "Max Kellerman") {
+        return accumulator + 1;
     }
-});
+    return accumulator;
+}, 0);
+
+execSync(`sed -i 's/Boxing%20references-[[:digit:]]\\+/Boxing%20references-${data.length}/g' README.md`)
+execSync(`sed -i 's/Max%20Kellerman%20boxing%20references-[[:digit:]]\\+/Max%20Kellerman%20boxing%20references-${numberOfMaxKellermanReferences}/g' README.md`);
+
+execSync(`git add README.md && git commit -m "Update boxing reference numbers in README.md"`);
