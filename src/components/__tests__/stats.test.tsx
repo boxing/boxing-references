1319b7cc492c78899558503842c315dcb059604b
Mike DiDomizio <mikedidomizio@gmail.com>
2021-07-20 17:59:00 -0400

    Refactor stats for maintainability

---
+++ src/components/__tests__/stats.test.tsx
@@ -0,0 +1,39 @@
+import Enzyme, { mount, ReactWrapper } from 'enzyme';
+import { data } from '../../data';
+import Stats from '../stats';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let wrapper: ReactWrapper;
+
+const props = {
+  songs: data,
+};
+
+beforeEach(() => {
+  wrapper = mount(<Stats {...props} />);
+});
+
+afterEach(() => {
+  wrapper.unmount();
+});
+
+test('should have 10 results', () => {
+  expect(wrapper.html().match(/\((\d+)\)/g)?.length).toBe(10);
+});
+
+test('should results sorted where elements go in order descending', () => {
+  const arr: IterableIterator<any> = wrapper.html().matchAll(/\(([\d+])\)/g);
+  // @ts-ignore
+  const arrNumbers: number[] = [...arr]
+    .map((i) => parseInt(i[1], 10))
+    .slice(0, 5);
+  expect(arrNumbers).toBeDefined();
+
+  for (let i = 0; i < arrNumbers.length - 1; i++) {
+    const num1 = arrNumbers[i];
+    const num2 = arrNumbers[i + 1];
+    expect(num1 >= num2).toBe(true);
+  }
+});
--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,71 +1,56 @@
 /*eslint no-sequences: 0*/
 
 import { song } from '../data';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
+
+type stat = [string, number][];
 
 function Stats(props: { songs: song[] }) {
-  const getPopularYears = (): any[] => {
-    const results = props.songs
-      .map((i) => i.year)
-      .reduce(
-        (prev: any, curr: any) => ((prev[curr] = ++prev[curr] || 1), prev),
-        {}
-      );
+  function mapAndReduceFn(mapFn: (string | number | undefined)[]): stat {
+    const results: [string, number][] = mapFn.reduce((acc: any, curr: any) => {
+      acc[curr] = ++acc[curr] || 1;
+      return acc;
+    }, {});
 
     return Object.entries(results).sort(
       (a: [string, any], b: [string, any]) => {
-        if (a[1] < b[1]) {
-          return 1;
-        }
-
-        if (a[1] > b[1]) {
-          return -1;
-        }
-
-        return 0;
+        return b[1] - a[1];
       }
-    );
+    ) as unknown as [string, number][];
+  }
+
+  const getPopularYears = (): stat => {
+    return mapAndReduceFn(props.songs.map((i) => i.year));
   };
 
-  const getPopularBoxers = (): any[] => {
-    const results = props.songs
-      .map((i) => i.boxer?.name)
-      .reduce(
-        (prev: any, curr: any) => ((prev[curr] = ++prev[curr] || 1), prev),
-        {}
-      );
-
-    return Object.entries(results).sort(
-      (a: [string, any], b: [string, any]) => {
-        if (a[1] < b[1]) {
-          return 1;
-        }
-
-        if (a[1] > b[1]) {
-          return -1;
-        }
-
-        return 0;
-      }
-    );
+  const getPopularBoxers = (): stat => {
+    return mapAndReduceFn(props.songs.map((i) => i.boxer?.name));
   };
 
-  const popularYears: any = getPopularYears();
-  const popularBoxers: any = getPopularBoxers();
-  const stat = (arg: any[]) => `${arg[0]} (${arg[1]})`;
+  const popularYears: [string, number][] = getPopularYears();
+  const popularBoxers: [string, number][] = getPopularBoxers();
+  const stat = (arg: (string | number)[]) => `${arg[0]} (${arg[1]})`;
 
   return (
     <div>
-      <Grid container justifyContent="center">
-        Top referenced years: {stat(popularYears[0])}, {stat(popularYears[1])},{' '}
-        {stat(popularYears[2])}, {stat(popularYears[3])},{' '}
-        {stat(popularYears[4])}
-      </Grid>
-      <Grid container justifyContent="center">
-        Top referenced boxers: {stat(popularBoxers[0])},{' '}
-        {stat(popularBoxers[1])}, {stat(popularBoxers[2])},{' '}
-        {stat(popularBoxers[3])}, {stat(popularBoxers[4])}
-      </Grid>
+      {props.songs.length > 0 && (
+        <div>
+          <Grid container justifyContent="center">
+            <Typography variant="body2">
+              Top referenced years: {stat(popularYears[0])},{' '}
+              {stat(popularYears[1])}, {stat(popularYears[2])},{' '}
+              {stat(popularYears[3])}, {stat(popularYears[4])}
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Typography variant="body2">
+              Top referenced boxers: {stat(popularBoxers[0])},{' '}
+              {stat(popularBoxers[1])}, {stat(popularBoxers[2])},{' '}
+              {stat(popularBoxers[3])}, {stat(popularBoxers[4])}
+            </Typography>
+          </Grid>
+        </div>
+      )}
     </div>
   );
 }
